99c6f3b62dc0de2cfd120545bf69e397b3a2a6b4
Dineshpyda <35535542+Dineshpyda@users.noreply.github.com>
2022-12-14 12:14:42 +0530

    Delete awsrdss directory

--- awsrdss/awsrds/connection.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-
-  region     = "us-east-2"
-  access_key = "AKIAWV7VLB4H5JH3WNUE"
-  secret_key = "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"
-
-}
--- awsrdss/awsrds/outputsmain.tf
+++
@@ -1,5 +0,0 @@
-
-output "arn_rdsaurora" {
-  description = "ARN from rds.tf aws_rds_cluster.rdsaurora"
-  value = module.rds.cluster_arn
-}
